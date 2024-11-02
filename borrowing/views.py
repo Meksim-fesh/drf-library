fc653047011b4f83ca1ef6ee64268c811885f444
Maksym Nimych <maksimnimitch@gmail.com>
2024-11-02 18:33:55 +0200

    Add borrowing views for list & detail

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import (
+    BorrowingDetailSerializer,
+    BorrowingListSerializer,
+)
+
+
+class BorrowingListView(generics.ListAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingListSerializer
+
+
+class BorrowingRetrieveView(generics.RetrieveUpdateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingDetailSerializer
