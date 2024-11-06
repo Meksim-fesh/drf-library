82191cb9937b259a05b79bfb8a1dff87e262cf40
Maksym Nimych <maksimnimitch@gmail.com>
2024-11-06 22:54:27 +0200

    Fix few issues in Payment model

--- borrowing/models.py
+++ borrowing/models.py
@@ -31,8 +31,8 @@ class Payment(models.Model):
     status = models.CharField(choices=StatusChoice, max_length=7)
     type = models.CharField(choices=TypeChoice, max_length=7)
     borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
-    session_url = models.URLField()
-    session_id = models.PositiveIntegerField()
+    session_url = models.URLField(max_length=516)
+    session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
