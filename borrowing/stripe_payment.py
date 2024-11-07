57546cfa7cd73a7190d1ed58c93d3f7acbbc7db1
Maksym Nimych <maksimnimitch@gmail.com>
2024-11-07 17:34:28 +0200

    Implement success url in create_checkout_session function

--- borrowing/stripe_payment.py
+++ borrowing/stripe_payment.py
@@ -1,7 +1,10 @@
 import stripe
 import os
 
+from rest_framework.reverse import reverse
+
 from borrowing.models import Borrowing, Payment
+from library_system.settings import BASE_URL
 
 
 stripe.api_key = os.environ.get("STRIPE_API_KEY", False)
@@ -12,6 +15,14 @@ def create_checkout_session(borrowing: Borrowing) -> stripe.checkout.Session:
     amount_of_days = amount_of_days.days
     price = borrowing.book.daily_fee * amount_of_days * 100
     stride_price = int(price)
+
+    data = {
+        "status": "Pending",
+        "type": "Payment",
+        "borrowing": borrowing,
+        "money_to_pay": price,
+    }
+    payment = Payment.objects.create(**data)
 
     session = stripe.checkout.Session.create(
         line_items=[
@@ -27,19 +38,15 @@ def create_checkout_session(borrowing: Borrowing) -> stripe.checkout.Session:
             }
         ],
         mode="payment",
-        success_url="http://localhost:4242/success",
+        success_url=(
+            f"{BASE_URL}"
+            f"{reverse("borrowing:payment-success", args=[payment.id])}"
+        ),
         cancel_url="http://localhost:4242/cancel",
     )
 
-    data = {
-        "status": "Paid",
-        "type": "Payment",
-        "borrowing": borrowing,
-        "session_url": session["url"],
-        "session_id": session["id"],
-        "money_to_pay": price,
-    }
-
-    Payment.objects.create(**data)
+    payment.session_url = session.url
+    payment.session_id = session.id
+    payment.save()
 
     return session
